58c737962d69db7fada496a643bf547e00960b54
Chad - ubuntu <scderya@gmail.com>
2020-12-07 18:56:44 +0000

    Removing unnecessary styling

--- src/Orders.js
+++ src/Orders.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import { DataGrid } from "@material-ui/data-grid";
 
 import Title from "./Title";
@@ -10,14 +9,7 @@ function createData(id, date, name, shipTo, paymentMethod, amount) {
   return { id, date, name, shipTo, paymentMethod, amount };
 }
 
-const useStyles = makeStyles((theme) => ({
-  seeMore: {
-    marginTop: theme.spacing(3),
-  },
-}));
-
 export default function Orders() {
-  const classes = useStyles();
   return (
     <React.Fragment>
       <Title>Pods</Title>
